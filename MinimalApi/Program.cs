6ad69ad020cf568ef1130397fd990df74893767b
daniyalaslam54 <daniyalaslam54@gmail.com>
2024-01-27 23:59:39 +0500

    added Register Endpoint Definitions

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -10,41 +10,6 @@ builder.RegisterServices();
 var app = builder.Build();
 
 app.UseHttpsRedirection();
-
-app.MapGet("api/post/{id:int}", async (int id, IMediator mediator) =>
-{
-    var getPost = new GetPostById() { PostId = id };
-    var post = await mediator.Send(getPost);
-    return Results.Ok(post);
-}).WithName("GetPostById");
-
-app.MapPost("api/posts", async (Post post, IMediator mediator) =>
-{
-    var createPost = new CreatePost() { PostContent = post.Content };
-    var createdPost = await mediator.Send(createPost);
-    return Results.CreatedAtRoute("GetPostById", new { createdPost.Id}, createdPost);
-});
-
-app.MapGet("api/posts", async (IMediator mediator) =>
-{
-    var getPosts = new GetAllPosts();
-    var post = await mediator.Send(getPosts);
-    return Results.Ok(post);
-}).WithName("GetPosts");
-
-app.MapPut("api/posts/{id:int}", async (int id, Post post,IMediator mediator) =>
-{
-    var updatePost = new UpdatePost() { Id = id , PostContent = post.Content};
-    var updatedPost = await mediator.Send(updatePost);
-    return Results.Ok(updatedPost);
-}).WithName("UpdatePost");
-
-app.MapDelete("api/post/{id:int}", async (int id, IMediator mediator) =>
-{
-    var deletePost = new DeletePost() { Id = id };
-    var deletedPost = await mediator.Send(deletePost);
-    return Results.NoContent();
-}).WithName("DeletePost");
-
+app.RegisterEndpointDefinitions();
 app.Run();
 
